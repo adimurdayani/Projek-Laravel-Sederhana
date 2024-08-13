9de85e3078ae886961c5d22ed0f12ad054996d4c
adimurdayani <adimurdayani@gmail.com>
2024-08-13 11:39:50 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use App\Http\Controllers\BerandaController;
 */
 
 Route::get('/', [BerandaController::class, 'index']);
-// Route::get('/', [BerandaController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::get('/user/edit/{user}', [UserController::class, 'edit']);
+Route::post('/user/aksiTambah', [UserController::class, 'aksiTambah']);
+Route::put('/user/aksiUpdate/{user}', [UserController::class, 'aksiUpdate']);
+Route::put('/user/hapus/{user}', [UserController::class, 'hapus']);
